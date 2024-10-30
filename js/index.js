364bb7328ea8d8d67fd34416bfe06b0c83b1094b
Assma Baziz <assma.baziz@yahoo.com>
2024-10-30 22:54:50 +0300

    some changes in logic

--- js/index.js
+++ js/index.js
@@ -3,19 +3,18 @@ const mainImg = document.querySelector(".main-img ");
 const image = document.querySelector(".main-img img");
 const btnPrev = document.getElementById("btnPrev");
 const btnNext = document.getElementById("btnNext");
-let containerThumnails = document.querySelector(".containerThumnails")
-containerThumnails.style.gridTemplateColumns = `40px 40px 40px 40px 40px 40px 40px 40px`
+let containerThumnails = document.querySelector(".containerThumnails");
+let imagesArray;
 let currentSlide = 0;
-
 mainImg.style.height = image.clientHeight + "px";
+containerThumnails.style.gridTemplateColumns = `repeat(${slideImages.length}, 1fr)`;
 uppdateSliderControl();
 function goToSlide(slideNumber) {
-  slideImages[currentSlide].style.opacity = "0%";
+  slideImages[currentSlide].style.opacity = "0";
   currentSlide = (slideNumber + slideImages.length) % slideImages.length;
-  slideImages[currentSlide].style.opacity = "100%";
-  console.log(currentSlide);
-  
+  slideImages[currentSlide].style.opacity = "1";
   uppdateSliderControl();
+  uppdatesStatusActive(currentSlide);
 }
 btnPrev.addEventListener("click", () => {
   goToSlide(currentSlide - 1);
@@ -25,12 +24,30 @@ btnNext.addEventListener("click", () => {
 });
 function uppdateSliderControl() {
   btnPrev.disabled = currentSlide == 0;
-  btnNext.disabled = currentSlide == slideImages.length -1;
-  // console.log("helllllllllllllllo");
+  btnNext.disabled = currentSlide == slideImages.length - 1;
 }
 slideImages.forEach((img, index) => {
-  const thumbImg = img.cloneNode()
-  console.log(thumbImg);
-  thumbImg.style.gridTemplateColumns = "40px"
-  // containerThumnails.appendChild(thumbImg) 
-})
+  const thumbImg = img.cloneNode();
+  thumbImg.classList.remove("absolute", "inset-0");
+  thumbImg.style.cursor = "pointer";
+  thumbImg.addEventListener("click", function () {
+    goToSlide(index);
+  });
+  containerThumnails.appendChild(thumbImg);
+  imagesArray = Array.from(containerThumnails.querySelectorAll("img"));
+});
+for (let i = 0; i < imagesArray.length; i++) {
+  if(i !== imagesArray.length -1) {
+    imagesArray[i].style.opacity = "50%";
+  }
+}
+function uppdatesStatusActive(index) {
+  containerThumnails.querySelectorAll("img").forEach((img, i) => {
+    img.style.opacity = "50%";
+    if (i === index) {
+      img.style.opacity = "100%";
+    } else {
+      img.style.opacity = "50%";
+    }
+  });
+}
